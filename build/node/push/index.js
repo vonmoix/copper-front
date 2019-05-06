0065b5780e2cf5e5d3c528e9b2b82f8ab111fd4c
ariiiman <aristide.benoist@gmail.com>
2019-05-06 17:28:17 +0200

    0.0.15

--- build/node/push/index.js
+++ build/node/push/index.js
@@ -4,6 +4,13 @@ const PackageJson = require('../../../' + PackageFile)
 const Readline = require('readline')
 const colors = require('colors')
 
+// Version reset
+const src = 'src/public/index.php'
+const content = fs.readFileSync(src, 'utf8')
+const versionCurr = +content.match(/VERSION\',\s(.*?)\);/)[1]
+const replacement = content.replace('VERSION\', ' + versionCurr + ')', 'VERSION\', 0)')
+fs.writeFileSync(src, replacement, 'utf-8')
+
 // Get package.json version
 const q = colors.blue('CURRENT VERSION : ' + PackageJson.version + '\r\n') + colors.yellow('NEW VERSION ?\r\n\r\n')
 const rl = Readline.createInterface({
